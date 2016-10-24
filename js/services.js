00ced991df766f23ab348bb31849e4a0832b41c3
Kristjan Gannon <gannonk08@gmail.com>
2016-10-24 15:29:18 -0600

    updated base url

--- js/services.js
+++ js/services.js
@@ -10,7 +10,7 @@
 
   function coffeeService($http) {
     /*jshint validthis: true */
-    const coffeeURL = 'http://localhost:8000/coffee';
+    const coffeeURL = 'http://sheltered-waters-74534.herokuapp.com/';
 
     this.getAllCoffee = function () {
       return $http.get(coffeeURL);
